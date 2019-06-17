71ddb0abea978fdc31f9230897aed8ad02e4eb30
sahil <sahilgupta@ucreate.co.in>
2019-06-17 14:12:12 +0530

    write test case for user instance

---
+++ tests/Unit/SessionTwoPracticeTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class SessionTwoPracticeTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testUserInstance()
+    {
+        $this->assertInstanceOf(User::class, $user);
+    }
+}
